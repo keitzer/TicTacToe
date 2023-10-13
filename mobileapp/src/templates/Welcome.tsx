e4e74d058d292f7d1b1f0d626de0b48b3a3bef31
Alex Ogorek <alex@alexogorek.com>
2023-10-12 21:04:23 -0700

    base game working

--- mobileapp/src/templates/Welcome.tsx
+++ mobileapp/src/templates/Welcome.tsx
@@ -1,9 +1,115 @@
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-const Welcome = () => (
-  <View className="flex-1 items-center justify-center">
-    <Text>Open up App/ to start working on your app!</Text>
-  </View>
-);
+interface BoardProps {
+  squares: string[];
+  onClick: (i: number) => void;
+}
+
+const Board: React.FC<BoardProps> = ({ squares, onClick }) => {
+  const renderSquare = (i: number) => {
+    return (
+      <View style={styles.square}>
+        <Button title={squares[i] ?? ""} onPress={() => onClick(i)} />
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.board}>
+      <View style={styles.row}>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </View>
+      <View style={styles.row}>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </View>
+      <View style={styles.row}>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </View>
+    </View>
+  );
+};
+
+const Welcome: React.FC = () => {
+  const [board, setBoard] = useState<string[]>(Array(9).fill(''));
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+
+  const calculateWinner = (squares: string[]) => {
+    const lines: number[][] = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i] as [number, number, number];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const handleClick = (i: number) => {
+    const squares = [...board];
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = xIsNext ? 'X' : 'O';
+    setBoard(squares);
+    setXIsNext(!xIsNext);
+  };
+
+  const winner = calculateWinner(board);
+  let status: string;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.status}>{status}</Text>
+      <Board squares={board} onClick={handleClick} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  status: {
+    marginBottom: 10,
+  },
+  board: {
+    flexDirection: 'column',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  square: {
+    width: 60,
+    height: 60,
+  },
+});
 
 export { Welcome };
