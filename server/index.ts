5c53d39317ea09567d6edab51d2874dfc5e3cd80
Alex Ogorek <alex@alexogorek.com>
2023-10-13 21:17:18 -0700

    server updated a ton i think -- node modules removed from remote

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,25 @@
-const { ApolloServer, gql } = require('apollo-server');
-const { createServer } = require('http');
-const { SubscriptionServer } = require('subscriptions-transport-ws');
-const { execute, subscribe } = require('graphql');
+import { ApolloServer } from '@apollo/server';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { PubSub, withFilter } from 'graphql-subscriptions';
+
+const pubsub = new PubSub();
 
 const games = {}; // In-memory storage for game data
 const subscribers = {}; // Subscribers for game updates
 
-// Define your GraphQL schema
-const typeDefs = gql`
-type Player {
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  type Player {
     id: ID!
     name: String!
   }
@@ -45,7 +56,8 @@ type Player {
   }
 `;
 
-// Define your resolvers
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
         games: () => Object.values(games),
@@ -83,7 +95,7 @@ const resolvers = {
                 throw new Error('Game is already full');
             }
 
-            const player = { id: playerId, username: `Player 2` };
+            const player = { id: playerId, name: `Player 2` };
             game.players.push(player);
 
             return game;
@@ -132,30 +144,71 @@ const resolvers = {
     },
 };
 
-// Create an instance of Apollo Server
-const server = new ApolloServer({ typeDefs, resolvers });
-
-async function startServer() {
-    const httpServer = createServer(server.getMiddleware());
-    server.installSubscriptionHandlers(httpServer);
-
-    await server.listen({ port: 4000 }).then(({ url, subscriptionsUrl }) => {
-        console.log(`Server ready at ${url}`);
-        console.log(`Subscriptions ready at ${subscriptionsUrl}`);
-    });
-
-    // Create a SubscriptionServer to handle WebSocket subscriptions
-    const subscriptionServer = new SubscriptionServer(
-        {
-            execute,
-            subscribe,
-            schema: server.schema,
-        },
-        {
-            server: httpServer,
-            path: server.subscriptionsPath,
-        }
-    );
+interface MyContext {
+    token?: string;
 }
 
-startServer();
+// Required logic for integrating with Express
+const app = express();
+
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = createServer(app);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Creating the WebSocket server
+const wsServer = new WebSocketServer({
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+    // Pass a different path here if app.use
+    // serves expressMiddleware at a different path
+    path: '/subscriptions',
+});
+
+// Hand in the schema we just created and have the
+// WebSocketServer start listening.
+const serverCleanup = useServer({ schema }, wsServer);
+
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
+const server = new ApolloServer<MyContext>({
+    schema,
+    plugins: [
+        // Proper shutdown for the HTTP server.
+        ApolloServerPluginDrainHttpServer({ httpServer }),
+
+        // Proper shutdown for the WebSocket server.
+        {
+            async serverWillStart() {
+                return {
+                    async drainServer() {
+                        await serverCleanup.dispose();
+                    },
+                };
+            },
+        },
+    ],
+});
+
+// Ensure we wait for our server to start
+await server.start();
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+    '/',
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+        context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+);
+
+const PORT = 4000;
+// Modified server startup
+await new Promise<void>((resolve) => httpServer.listen(PORT, resolve));
+console.log(`🚀 Server ready at http://localhost:${PORT}/`);
